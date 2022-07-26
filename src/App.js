afa325a2c149b0ba397e39e2d9e6938787d3dae1
jkang4531 <jkang4531@naver.com>
2022-07-26 10:40:18 +0900

    Simple ToDo List
    setToDos((currentArray) => [toDo, ...currentArray]);
    스프레드 사용
    currentArray = 새로운 배열을 만들어서
    내 생각에 비어있는 toDos랑 currentArray랑 같은거 같음
    
    toDos에 있는 배열을 map을 사용해서 li로 뿌려줌

--- src/App.js
+++ src/App.js
@@ -13,11 +13,17 @@ function App() {
       // 비어있으면 작동하지 않도록
       return;
     }
+    // currentArray = 새로운 배열을 만들어서
+    // 내 생각에 비어있는 toDos랑 currentArray랑 같은거 같음
+    setToDos((currentArray) => [toDo, ...currentArray]);
     // 안비어있고 엔터치면 input이 비어있을 수 있도록
     setToDo("");
   };
+  console.log(toDos);
+
   return (
     <div className="App">
+      <h1>My To Dos ({toDos.length})</h1>
       <form onSubmit={onSubmit}>
         <input
           onChange={onChange}
@@ -27,6 +33,13 @@ function App() {
         />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+          // map으로 배열의 요소를 하나씩(item) 가져와서 li에 추가
+        ))}
+      </ul>
     </div>
   );
 }
